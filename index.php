b3667ad577451dea59a937e5d152c7b156656676
Mariann Adelbert <mariannadelbert@gmail.com>
2025-03-07 10:03:47 +0200

    add box example

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 // comments are this way
-
 /*
-plapla
-plapla
-*/
-
 echo 'Hello World ';
 
 $var = 'Mariann'; //string
@@ -43,3 +38,31 @@ hello('Mariann');
 
 $name = 'Mari-Ann';
 echo " don't be \n an a** $name";
+*/
+
+// <?php see peab ka alguses olema
+
+class Box {
+    public $width;
+    public $height;
+    public $length;
+
+    public function volume(){
+        return $this-> width * $this-> height * $this-> length;
+    }
+}
+
+$box1 = new Box();
+$box1->width = 10;
+$box1->height = 20;
+$box1->length = 30;
+var_dump($box1);
+var_dump($box1->volume());
+
+$box2 = new Box();
+$box2->width = 40;
+$box2->height = 60;
+$box2->length = 50;
+var_dump($box2);
+var_dump($box2->volume());
+var_dump($box1);
